53d3a4885f416a6238842bcf648266bf5b7b4916
Sandeep77GT <Sandeep.ds24@duk.ac.in>
2025-05-24 20:11:26 +0530

    Update job_app.py

--- job_app.py
+++ job_app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1kJKtpvTqf8uEEL_OtXWvUf8s3rLodI9W
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 
